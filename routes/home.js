ee749daec9835b6970bff748032a06f12ce8b65b
ALVA777888 <hhhhhhhhhccccc.bbbbbdddd@gmail.com>
2024-10-27 23:07:12 +0900

    home追加

--- routes/home.js
+++ routes/home.js
@@ -1,1 +1,33 @@
-//XでいうところのHome画面の情報をGetする場所にしようかなと考えている
+// const router = require("express").Router();
+// const express = require("express");
+// const checkJWT = require("../middleware/checkJWT");
+// const { UserPost } = require("../db/User");
+
+// //home画面のTLを取得(認証必要)
+// router.get("/", checkJWT, async (req, res) => {
+//     try {
+//         const userId = req.user.userid; //JWTからuseridを取得
+//         //特定のpostを取得、日時で降順にソート
+//         const posts = await UserPost.find({ userid: userId }).sort({ posttime: -1 }).limit(10);
+
+//         return res.json(posts);
+//     } catch (error) {
+//         console.error("Error home timeline", error);
+//         return res.status(500).json({ message: "TimeLineの取得中にエラーが発生しました。"});
+//     }
+// });
+
+// //最新投稿を取得(デバッグ、user向け)
+// router.get("/recent", async (req, res) => {
+//     try {
+//         //最新10件取得、降順ソート
+//         const recentPosts = await UserPost.find().sort({ posttime: -1 }).limit(10);
+
+//         return res.json(recentPosts);
+//     } catch (error) {
+//         console.error("Error recent posts:", error);
+//         return res.status(500).json({ message: "最新投稿の取得時にエラーが発生しました。"});
+//     }
+// });
+
+// module.exports = router;
--- server.js
+++ server.js
@@ -4,15 +4,19 @@ const auth = require("./routes/auth");
 const post = require("./routes/post");
 const config = require("./config");
 const mongoose = require("mongoose");
+// const home = require("./routes/home");
 
 //大元のサーバー部分
 
 const PORT = 3000; //ローカルで使用するPORTを指定
-mongoose.connect(config.database.url).then(() => console.log("Database connected")).catch((err) => console.log(err));//DB接続
+mongoose.connect(config.database.url)
+    .then(() => console.log("Database connected"))
+    .catch((err) => console.log(err));//DB接続
 
 app.use(express.json());//サーバーでJsonを使えるように設定
 app.use("/auth", auth);//authを指定してWebAPIを構築できるようにする
 app.use("/post", post);
+// app.use("/home", home);
 
 
 app.listen(PORT, () => {
